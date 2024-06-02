ca72c324d1d5b2b163b6776387b6c4cae3773b59
Your Name <you@example.com>
2024-06-02 22:26:37 +0800

    v4_done_v5_unsafe_alpha

--- ttc/ConsoleApp1/ConsoleApp1/obj/Debug/net8/ConsoleApp1.AssemblyInfo.fs
+++ ttc/ConsoleApp1/ConsoleApp1/obj/Debug/net8/ConsoleApp1.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b4121221d2dfad56564c3f103c9347521e827c")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdd46e2df7841519ae00bd45de5bacb93093958a")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
